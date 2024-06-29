c7b1a6a99527ced4af54b90eeb34fe7daa3d49ad
dimasamehada <dimasamehada@localhost.localdomain>
2024-06-29 19:32:22 +0200

    add config variables to main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.api.api_router import api_router
+from app.core.config import settings
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host=settings.SERVER_HOST,
+        port=settings.SERVER_PORT,
+        reload=True,
+    )
