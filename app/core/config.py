b4f81261f7fb9a180e3ef39e12f0ceb0daee2138
dimasamehada <dimasamehada@localhost.localdomain>
2024-07-01 18:12:08 +0200

    update env variables and docker-compose

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     DB_URI = os.getenv("DB_URI")
-    REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+    REDIS_URL = os.getenv("REDIS_URL")
 
     SECRET_KEY = os.getenv("SECRET_KEY")
     HASH_ALGORITHM = "HS256"
@@ -15,13 +15,10 @@ class Config:
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
     REFRESH_TOKEN_EXPIRE_DAYS = 30
 
-    SERVER_HOST = os.getenv("SERVER_HOST", "0.0.0.0")
-    SERVER_PORT = int(os.getenv("SERVER_PORT", "8000"))
+    SERVER_HOST = os.getenv("SERVER_HOST")
+    SERVER_PORT = int(os.getenv("SERVER_PORT"))
 
-    AI_API_KEY = os.getenv(
-        "AI_API_KEY",
-        "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",
-    )
+    AI_API_KEY = os.getenv("AI_API_KEY")
     AI_MODEL = "llama3-8b-8192"  # Another models - https://console.groq.com/docs/models
     AI_PROMPT = (
         "Imagine you're author of the post {post}. Some user left a comment on your post: {comment}."
